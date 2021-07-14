a3644c85d945e24d3629a1f453b52f9c51e53cc4
Ross Edwards <rossedwards.us@gmail.com>
2021-07-14 16:38:37 -0400

    Fixed naming

---
+++ src/CalulatedValueComponent.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+
+import styled from "styled-components";
+
+
+const CalculatedValueComponent = styled(TextField)``;
+
+
+export default CalculatedValueCompoent;
+
---
+++ src/Input2ValueComponent.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+import styled from "styled-components";
+
+import TextField from '@material-ui/core/TextField';
+
+
+const Input2ValueComponent = styled(TextField)``;
+
+
+
+export default Input2ValueComponent;
+
+
